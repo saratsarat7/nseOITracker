cadee07de05b372649d01fb8c8df8681ed80d49a
Sarat <31168235+saratsarat7@users.noreply.github.com>
2021-10-21 17:19:43 +0530

    update to OS library

--- get_data.py
+++ get_data.py
@@ -23,6 +23,7 @@ def loadToMongo(load_file):
     df['Percent'] = (df[" Open Interest"] / df[" MWPL"])*100
     
     uri = "mongodb+srv://"+config('MONGO_USER')+":"+config('MONGO_PASSWORD')+"@iovalues.lgijy.mongodb.net/iovalues?retryWrites=true&w=majority"
+    uri = "mongodb+srv://"+os.environ.get('MONGO_USER')+":"+os.environ.get('MONGO_PASSWORD')+"@iovalues.lgijy.mongodb.net/iovalues?retryWrites=true&w=majority"
     
     print(config('MONGO_USER'))
     print(config('MONGO_PASSWORD'))
